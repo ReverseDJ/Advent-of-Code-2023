62427676cfcd37211a9c682c1451e0bae96e36f5
Justin Dhillion <jsdhilli@ncsu.edu>
2023-12-15 17:21:42 -0500

    Finished Day 1

--- Day 1/main.cc
+++ Day 1/main.cc
@@ -6,23 +6,25 @@ using namespace std;
 int main(){
     FILE *FP;
 	FP = fopen("inputs.txt", "r");
-    char * input;  // Adjust the size based on your input length
+    char input [50];  // Adjust the size based on your input length
     int first = 0, second = 0, first_index, second_index;
+    int tens, ones;
+    int sum=0;
 
     if (FP) {
         while (fscanf(FP, "%s", input) != EOF) {
             int length = strlen(input);
             for(int i=0;i<length;i++){
                 if (input[i]>='0' && input[i]<='9'){
-                    first = atoi(&input[i]);
+                    first = input[i] - 48;
                     first_index = i;
                     break;
                 }
             }
             for(int j=length-1;j>first_index;j--){
-                if (input[j]>=48 && input[j]<=57){
-                    input[j]=second;
-                    second_index=j;
+                if (input[j]>='0' && input[j]<='9'){
+                    second = input[j] - 48;
+                    second_index = j;
                     break;
                 }
             }
@@ -30,8 +32,13 @@ int main(){
                 second=first;
             }
             cout<<first<<second<<endl;
-            
+            tens=10*first;
+            ones=second;
+            sum += tens + ones;
+            first=0;
+            second=0;
             }
+            cout<<"Sum equals: "<<sum<< endl;
             fclose(FP);
         }
         
